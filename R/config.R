abccf6f0b13055a9c05260bf19fff682c016d416
Jeroen <jeroenooms@gmail.com>
2013-07-15 12:34:50 +0200

    added config files

--- R/config.R
+++ R/config.R
@@ -1,20 +1,22 @@
-config <- function(x){
-  switch(x,
-    "job.timeout" = 60,
-    "time.limit" = 90,
-    "gist.cache" = 5*60,
-    "github.cache" = 60*60,
-    "httpcache.post" = 5*60,
-    "httpcache.lib" = 60*60*24,  
-    "httpcache.git" = 60*60,
-    "httpcache.tmp" = 60*60*24,        
-    "httpcache.pages" = 60*60*24,          
-    "session.prefix" = "ocpu_tmp_",
-    "appspaths" = c("/usr/local/lib/opencpu/apps-library"),
-    "repos" = "http://cran.rstudio.com",
-    "tmpdir" = "/tmp",
-    "libpaths" = list(),
-    "preload" = list(),
-    stop("invalid config entry: ", x)
-  );
-}
+config <- local({
+  conflist <- list()
+  
+  load <- function(filepath){
+    newconf <- as.list(fromJSON(filepath));
+    for(i in seq_along(newconf)){
+      name <- names(newconf[i]);
+      conflist[[name]] <<- newconf[[i]];
+    }
+  }
+  
+  get <- function(x){
+    value = conflist[[x]];
+    if(is.null(value)){
+      stop("System error! No config set for: ", x);
+    }
+    return(value);
+  }
+  
+  #default is get
+  get
+})
--- R/onLoad.R
+++ R/onLoad.R
@@ -3,6 +3,23 @@ packagename = "";
 .onLoad <- function(path, package){
   packagename <<- package;
   
+  #load default package config file
+  defaultconf <- system.file("config/defaults.conf", package=package);
+  stopifnot(file.exists(defaultconf));
+  environment(config)$load(defaultconf);
+  
+  #override with system config file
+  if(file.exists("/etc/opencpu/server.conf")){
+    environment(config)$load("/etc/opencpu/server.conf");    
+  }
+  
+  #override with custom system config files
+  if(isTRUE(file.info("/etc/opencpu/server.conf.d")$isdir)){
+    conffiles <- list.files("/etc/opencpu/server.conf.d", full.names=TRUE, pattern=".conf$");
+    lapply(as.list(conffiles), environment(config)$load);
+  }
+  
+  #set some global options
   options(max.print=50);
   options(repos=config('repos'));
   options(keep.source = FALSE);
