7b1168f09527181ea3f97209bf2844b609a5cdc3
Jeroen <jeroenooms@gmail.com>
2013-07-15 17:32:21 +0200

    Small fix for windows

--- R/serve.R
+++ R/serve.R
@@ -3,18 +3,20 @@ serve <- function(REQDATA){
   #detect OS
   OS <- .Platform$OS.type;
   
-  #for GET requests we use the main proc
+  #for GET requests we use the main process
   if(identical(OS, "windows") && (REQDATA$METHOD %in% c("HEAD", "GET"))){
     return(request(eval_current(main(REQDATA), timeout=config("time.limit"))));   
   } 
   
   #for non GET we use a psock process
   if(identical(OS, "windows")){
-    #return(request(main(REQDATA)));
-    return(request(eval_psock(ocpu:::main(REQDATA), timeout=config("time.limit"))));   
+    #we use another trycatch block because request happens inside psockcluster
+    return(tryCatch({
+      eval_psock(ocpu:::request(ocpu:::main(REQDATA)), timeout=config("time.limit"));
+    }, error = reshandler));
   } 
   
   #On unix we always fork:
-  #eval_psock(ocpu:::request(ocpu:::main(REQDATA)), timeout=config("time.limit"))
+  #Note that fork happens inside request() instead of other way around.
   request(eval_fork(main(REQDATA), timeout=config("time.limit")));
 }
