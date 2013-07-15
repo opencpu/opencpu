5b9859e02fa6c5cbcc91aebcd245bd7049f06a8b
Jeroen <jeroenooms@gmail.com>
2013-07-15 15:40:36 +0200

    move fork inside of trycatch

--- R/serve.R
+++ R/serve.R
@@ -5,16 +5,16 @@ serve <- function(REQDATA){
   
   #for GET requests we use the main proc
   if(identical(OS, "windows") && (REQDATA$METHOD %in% c("HEAD", "GET"))){
-    return(eval_current(request(main(REQDATA)), timeout=config("time.limit")));   
+    return(request(eval_current(main(REQDATA), timeout=config("time.limit"))));   
   } 
   
   #for non GET we use a psock process
   if(identical(OS, "windows")){
     #return(request(main(REQDATA)));
-    return(eval_psock(ocpu:::request(ocpu:::main(REQDATA)), timeout=config("time.limit")));   
+    return(request(eval_psock(ocpu:::main(REQDATA), timeout=config("time.limit"))));   
   } 
   
   #On unix we always fork:
   #eval_psock(ocpu:::request(ocpu:::main(REQDATA)), timeout=config("time.limit"))
-  eval_fork(request(main(REQDATA)), timeout=config("time.limit"));
+  request(eval_fork(main(REQDATA), timeout=config("time.limit")));
 }
