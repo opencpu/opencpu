ecfd026ed159f1d8a24f595c5426bdde13f0ade9
Jeroen <jeroenooms@gmail.com>
2013-07-21 14:43:24 +0200

    fix for windoooz

--- R/eval_current.R
+++ R/eval_current.R
@@ -1,8 +1,21 @@
 eval_current <- function(expr, envir=parent.frame(), timeout=60){  
   #set the timeout
   setTimeLimit(elapsed=timeout, transient=TRUE);
+  
+  #currently loaded packages
+  currentlyloaded <- search();
+  
   on.exit({
+    #reset time limit
     setTimeLimit(cpu=Inf, elapsed=Inf, transient=FALSE);
+    
+    #try to detach packages that were attached during eval
+    newlyloaded <- search();
+    todetach <- newlyloaded[!(newlyloaded %in% currentlyloaded)];
+    for(i in seq_along(todetach)){
+      try(detach(todetach[i], unload=TRUE));
+    }    
+    
   });
   
   eval(expr, envir) 
