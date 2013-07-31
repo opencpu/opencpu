d7f95ced9d26172e78cc1df7bfe37e3ec09bd77e
Jeroen <jeroenooms@gmail.com>
2013-07-31 18:58:23 +0200

    user fix

--- R/userlibpath.R
+++ R/userlibpath.R
@@ -1,14 +1,14 @@
 #doesn't work on windows. R_LIBS_USER will always be logged in user.
 userlibpath <- function(username, postfix=""){
-  userhome <- file.path(userhome(), username);
-  homelib <- sub("~", userhome, Sys.getenv("R_LIBS_USER"));
+  userhomepath <- file.path(userhome(), username);
+  homelib <- sub("~", userhomepath, Sys.getenv("R_LIBS_USER"));
   homelib <- gsub("/+$", "", homelib);
   homelib <- paste(homelib, postfix, sep="");  
   return(homelib);
 }
 
 userhome <- function(){
-  if(Sys.info()[["effective_user"]] == "root"){
+  if(Sys.info()[["effective_user"]] %in% c("root", "www-data")){
     return("/home")
   } else {
     return(dirname(path.expand("~")));
