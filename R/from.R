51e0da8685cc6137a29a1c47b96ddd59c0980573
Jeroen <jeroenooms@gmail.com>
2013-08-24 12:45:15 +0200

    hacks

--- R/from.R
+++ R/from.R
@@ -1,4 +1,4 @@
-from = function (pkg, name) {
+from <- function (pkg, name) {
   pkg <- as.character(substitute(pkg))
   name <- as.character(substitute(name))
   get(name, envir = asNamespace(pkg), inherits = FALSE)
