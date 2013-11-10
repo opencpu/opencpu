87b464873223f61b42e6a06fa7efb09b0dd42de3
Jeroen Ooms <jeroenooms@gmail.com>
2013-11-09 17:19:24 -0800

    Support for system users.

--- R/httpget_user_library.R
+++ R/httpget_user_library.R
@@ -1,6 +1,11 @@
 httpget_user_library <- function(username, requri){
   #append user home library to path
   homelib <- userlibpath(username);
+  
+  #check if exists
+  if(!file.exists(homelib)){
+    res$error(paste("R package library for user", username, "not found."), 404);
+  }
 
   #load package from homelib (dependencies can still be loaded from site library)  
   inlib(homelib, {
