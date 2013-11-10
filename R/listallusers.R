febb3dc279f640abb38cd1bb6abcd3993e43f60c
Jeroen <jeroenooms@gmail.com>
2013-11-10 10:38:34 -0800

    better method for finding home dir

--- R/listallusers.R
+++ R/listallusers.R
@@ -1,4 +1,10 @@
 listallusers <- function(user.only=TRUE){
+  if(grepl("darwin", R.Version()$platform)){
+    out <- system2("dscl", ". list /Users | grep -v ^_.*", stdout=TRUE)
+    if(is.null(attr(out, "status")) || attr(out, "status") == 0){
+      return(out);
+    }
+  }
   if(file.exists("/etc/passwd")){
     out <- try(read.table("/etc/passwd", sep=":", as.is=TRUE));
     if(!is(out, "try-error") && length(out) && nrow(out)){
--- R/userlibpath.R
+++ R/userlibpath.R
@@ -1,8 +1,6 @@
 #doesn't work on windows. R_LIBS_USER will always be logged in user.
 userlibpath <- function(username, postfix=""){
-  usertable <- read.table("/etc/passwd", sep=":", as.is=TRUE, row.names=1);
-  userhome <- usertable[username,"V6"];
-  homelib <- sub("~", userhome, Sys.getenv("R_LIBS_USER"));
+  homelib <- path.expand(sub("~", paste0("~", username), Sys.getenv("R_LIBS_USER"), fixed=TRUE));
   homelib <- gsub("/+$", "", homelib);
   homelib <- paste(homelib, postfix, sep="");
   if(file.exists(homelib)){
