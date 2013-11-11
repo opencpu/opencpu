03b4068831549830111eab694fd732b73b8f7cc6
Jeroen Ooms <jeroenooms@gmail.com>
2013-11-11 10:32:45 -0800

    Use setContentType instead of setHeader (fix for old versions of Apache2)

--- R/rapachehandler.R
+++ R/rapachehandler.R
@@ -18,7 +18,7 @@ rapachehandler <- function(){
     NEWPOST[names(NEWFILES)] <- NULL;    
   }
   
-	#collect request data from rapache
+  #collect request data from rapache
   REQDATA <- list(
     METHOD = getrapache("SERVER")$method,
     MOUNT = getrapache("SERVER")$cmd_path,
@@ -29,13 +29,13 @@ rapachehandler <- function(){
     RAW = MYRAW
   );
     
-	#select method to parse request in a trycatch 
+  #select method to parse request in a trycatch 
   tmpnull <- tempfile();
-	sink(tmpnull);
-	response <- serve(REQDATA);
-	sink();
+  sink(tmpnull);
+  response <- serve(REQDATA);
+  sink();
   unlink(tmpnull);
-	
+  
   #set server header  
   response$headers["X-ocpu-server"] <- "rApache";      
 
@@ -45,12 +45,16 @@ rapachehandler <- function(){
   #set headers
   headerlist <- response$headers;
   for(i in seq_along(headerlist)){
-    getrapache("setHeader")(names(headerlist[i]), headerlist[[i]]);    
+    if(names(headerlist[i]) == "Content-Type"){
+      setContentType(headerlist[[i]]);
+    } else {
+      getrapache("setHeader")(names(headerlist[i]), headerlist[[i]]);          
+    }
   }
-	  
+    
   #send buffered body
   getrapache("sendBin")(readBin(response$body,'raw',n=file.info(response$body)$size));
 
   #return
-	return(getrapache("OK"));
+  return(getrapache("OK"));
 }
