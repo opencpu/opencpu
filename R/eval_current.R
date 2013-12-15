f61642ff26666c380a7c04bceeaaa0a4a08757be
Jeroen Ooms <jeroenooms@gmail.com>
2013-12-15 14:36:22 -0800

    fix

--- R/eval_current.R
+++ R/eval_current.R
@@ -4,6 +4,7 @@ eval_current <- function(expr, envir=parent.frame(), timeout=60){
   
   #currently attached packages
   attached_before <- search();
+  loaded_before <- loadedNamespaces();
   
   on.exit({
     #reset time limit
@@ -11,13 +12,12 @@ eval_current <- function(expr, envir=parent.frame(), timeout=60){
     
     #try to detach packages that were attached during eval
     attached_after <- search();
-    loaded_after <- loadedNamespaces();
     
     #only deals only with attaching, not unloading
     for(pkg in attached_after){
       if(pkg %in% attached_before){
         next;
-      } else if(sub("package:", "", pkg) %in% loaded_after) {
+      } else if(sub("package:", "", pkg) %in% loaded_before) {
         try(detach(pkg, unload=FALSE, character.only=TRUE, force=TRUE));
       } else {
         try(detach(pkg, unload=TRUE, character.only=TRUE, force=TRUE));        
