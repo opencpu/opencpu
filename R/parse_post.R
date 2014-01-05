99cb6bb11ae493ddd59ea3b854ab66da72c43428
Jeroen Ooms <jeroenooms@gmail.com>
2014-01-05 15:21:21 -0800

    raise error for no content type

--- R/parse_post.R
+++ R/parse_post.R
@@ -1,11 +1,16 @@
 parse_post <- function(reqbody, contenttype){
   #check for no data
-  if(is.null(reqbody) || !length(reqbody)){
+  if(!length(reqbody)){
     return(list())  
   }
-  
+
   #strip title form header
   contenttype <- sub("Content-Type: ?", "", contenttype, ignore.case=TRUE);
+  
+  #invalid content type
+  if(!length(contenttype) || !nchar(contenttype)){
+    stop("No Content-Type header found.")  
+  }
   
   #test for multipart
   if(grepl("multipart/form-data", contenttype, fixed=TRUE)){
