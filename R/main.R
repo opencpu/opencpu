ea0063ca3415077b465d14bcf7d7a4c3d5e5a965
Jeroen Ooms <jeroenooms@gmail.com>
2014-01-20 10:49:52 -0800

    fix for rapache

--- R/main.R
+++ R/main.R
@@ -2,9 +2,9 @@ main <- function(REQDATA){
   #load config (in case of eval_psock)
   loadconfigs();
   
-  #set a seed
-  myseed <- (unclass(Sys.time())*1e9*Sys.getpid()) %% 1e9;
-  set.seed(myseed);
+  #for some reason this creates RNG collisions on rapache
+  #myseed <- (unclass(Sys.time())*1e9 + Sys.getpid()) %% 1e9;
+  #set.seed(myseed);
 
   #To be sure. Note that POST requests will eventually switch to a session dir.
   setwd(tempdir()); 
