7ad43fc924c90a4be7c13c23b4d0e119828b6b98
Jeroen <jeroenooms@gmail.com>
2014-04-15 17:00:43 -0700

    fix for stop()

--- R/stop.R
+++ R/stop.R
@@ -1,4 +1,11 @@
 #changes default to call.=FALSE
 stop <- function(..., call. = FALSE, domain = NULL){
-  base::stop(..., call. = call., domain = domain);
+  args <- list(...)
+  if(length(args) == 1L && inherits(args[[1L]], "condition")){
+    #when error is a condition object
+    base::stop(args[[1L]])
+  } else{
+    #when error is a string
+    base::stop(..., call. = call., domain = domain);    
+  }
 }
