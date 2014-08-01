5c1140ef434078cd99867b0377f42a364909b98c
Jeroen Ooms <jeroenooms@gmail.com>
2014-08-01 21:52:37 +0200

    Let's try this

--- R/onLoad.R
+++ R/onLoad.R
@@ -3,6 +3,14 @@ packagename = "";
 .onLoad <- function(lib, pkg){
   packagename <<- pkg;
   
+  if(isTRUE(getOption("rapache"))){
+    is_rapache(TRUE)
+  }
+  
+  if(isTRUE(getOption("apparmor"))){
+    has_apparmor(TRUE)
+  }
+  
   #Makes sure methods is loaded, which should always be the case
   #This is needed for R CMD CHECK only... looks like a bug?
   #See Uwe @ https://stat.ethz.ch/pipermail/r-devel/2011-October/062261.html
