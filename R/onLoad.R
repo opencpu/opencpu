ef0050ffce2bc2976dd7e9abdc5dfb93e93df73b
Jeroen Ooms <jeroenooms@gmail.com>
2014-08-12 00:26:47 +0200

    Comment out workaround for CMD check bug

--- R/onLoad.R
+++ R/onLoad.R
@@ -18,5 +18,5 @@ packagename = "";
   #Makes sure methods is loaded, which should always be the case
   #This is needed for R CMD CHECK only... looks like a bug?
   #See Uwe @ https://stat.ethz.ch/pipermail/r-devel/2011-October/062261.html
-  eval(call("require", "methods", quietly=TRUE))
+  #eval(call("require", "methods", quietly=TRUE))
 }
