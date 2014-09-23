09ff6ee79a3eff072e8ababc5f938e4992833df7
Jeroen Ooms <jeroenooms@gmail.com>
2014-09-23 17:33:50 +0200

    Add accept-encoding header to CORS

--- R/request.R
+++ R/request.R
@@ -59,7 +59,7 @@ reshandler <- function(e){
   #add CORS header
   if(isTRUE(config("enable.cors"))){
     response$headers[["Access-Control-Allow-Origin"]] = "*";
-    response$headers[["Access-Control-Allow-Headers"]] = "Origin, Content-Type, Accept, Cache-Control";
+    response$headers[["Access-Control-Allow-Headers"]] = "Origin, Content-Type, Accept, Accept-Encoding, Cache-Control";
     response$headers[["Access-Control-Expose-Headers"]] = "Location, X-ocpu-session, Content-Type, Cache-Control";    
   }
   
