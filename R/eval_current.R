433f41ccc622fc6a9bdeb8d16f2f8afce3a9045a
Jeroen Ooms <jeroenooms@gmail.com>
2017-05-01 19:22:47 +0200

    Move function into utils

--- R/eval_current.R
+++
@@ -1,5 +0,0 @@
-eval_current <- function(expr, envir=parent.frame(), timeout=60){  
-  setTimeLimit(elapsed=timeout, transient=TRUE);
-  on.exit(setTimeLimit(cpu=Inf, elapsed=Inf, transient=FALSE))
-  eval(expr, envir) 
-}
--- R/utils.R
+++ R/utils.R
@@ -137,3 +137,9 @@ stub <- function(){
   httpuv::runServer()
   knitr::knit()
 }
+
+eval_current <- function(expr, envir=parent.frame(), timeout=60){
+  setTimeLimit(elapsed=timeout, transient=TRUE);
+  on.exit(setTimeLimit(cpu=Inf, elapsed=Inf, transient=FALSE))
+  eval(expr, envir)
+}
