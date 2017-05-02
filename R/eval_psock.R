6f551d7521b2f7822524cd7824718486281bf7d2
Jeroen Ooms <jeroenooms@gmail.com>
2017-05-02 16:45:46 +0200

    Clean up eval_psock()

--- R/eval_psock.R
+++ R/eval_psock.R
@@ -1,51 +1,35 @@
-##NOTE: the timeout works in rstudio and rgui on win, but it doesn't work in rterminal.
-eval_psock <- function(expr, envir=parent.frame(), timeout=60, opts){
-  
+eval_psock <- function(expr, envir = parent.frame(), timeout = 60){
+  # imports
+  sendCall <- getFromNamespace('sendCall', 'parallel')
+  recvResult <- getFromNamespace('recvResult', 'parallel')
+
   #create a child process
-  cluster <- parallel::makePSOCKcluster(1);
-  child <- cluster[[1]];
-  from("parallel", "sendCall")(child, eval, list(quote(Sys.getpid())));
-  pid <- from("parallel", "recvResult")(child);
-  
+  cluster <- parallel::makePSOCKcluster(1)
+  child <- cluster[[1]]
+  sendCall(child, eval, list(quote(Sys.getpid())))
+  pid <- recvResult(child)
+
   #set the timeout
-  setTimeLimit(elapsed=timeout, transient=TRUE);
+  setTimeLimit(elapsed = timeout, transient = TRUE)
   on.exit({
-    setTimeLimit(cpu=Inf, elapsed=Inf, transient=FALSE);
-    tools::pskill(pid); #win
-    tools::pskill(pid, tools::SIGKILL); #nix
-    from("parallel", "stopNode")(child);
-  });
-  
-  #try to set options
-  if(!missing(opts)){
-    from("parallel", "sendCall")(child, eval, list(quote(options(opts)), envir=list(opts=as.list(opts))));
-    from("parallel", "recvResult")(child);    
+    setTimeLimit(cpu = Inf, elapsed = Inf, transient = FALSE)
+    parallel::stopCluster(cluster)
+    tools::pskill(pid) # Just in case
+  })
+
+  #send the actual call. Make sure that packages get loaded.
+  sendCall(child, eval, list(expr=substitute(expr), envir=as.list(envir)))
+  myresult <- recvResult(child)
+
+  #raise error. Should not happen when call has been wrapped in request()
+  if(inherits(myresult, "try-error")){
+    stop(myresult)
   }
-  
-  #send the actual call
-  #package/objects are already loaded??
-  from("parallel", "sendCall")(child, eval, list(expr=substitute(expr), envir=as.list(envir)));
-  myresult <- from("parallel", "recvResult")(child);
-  
-  #reset timelimit
-  setTimeLimit(cpu=Inf, elapsed=Inf, transient=TRUE);
-  
-  #forks don't throw errors themselves
-  if(inherits(myresult,"try-error")){
-    #snow only returns the message, not an error object
-    stop(myresult, call.=FALSE);
-  }
-  
-  #send the buffered response
-  return(myresult);   
+  return(myresult)
 }
 
-# test <- function(){
-#   n <- 1e8;
-#   k <- 1e4;
-#   #this should take more than 10 sec
-#   eval_psock(svd(matrix(rnorm(n), k)), timeout=10);
-# }
-# 
-# system.time(test());
-
+# should take more than 5 sec
+test_eval_psock <- function(len = 10000){
+  n <- len^2
+  eval_psock(svd(matrix(rnorm(n), len)), timeout = 5);
+}
