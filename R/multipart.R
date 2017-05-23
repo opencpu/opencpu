107858270827ff3bd20e0d8eb9b886c76f22c0ec
Jeroen Ooms <jeroenooms@gmail.com>
2017-05-23 21:14:27 +0200

    Add support for binary multipart types RDS and rprotobuf

--- R/multipart.R
+++ R/multipart.R
@@ -9,6 +9,17 @@ multipart <- function(body, type){
         name = x$filename,
         tmp_name = tmp
       )
+    } else if(length(x$content_type)){
+      # binary form-data objects that are not file uploads
+      if(identical(x$content_type, "application/rds")){
+        I(unserialize(x$value))
+      } else if(identical(x$content_type, "application/rprotobuf")){
+        I(protolite::unserialize_pb(x$value))
+      } else if(grepl("^text/", x$content_type)){
+        I(rawToChar(x$value))
+      } else {
+        I(x$value)
+      }
     } else if(is.raw(x$value)){
       rawToChar(x$value)
     } else {
--- teststuff/test-api.R
+++ teststuff/test-api.R
@@ -1,14 +1,56 @@
 # Generic client
-ocpu <- function(path, ..., server = 'http://localhost:5656/ocpu'){
+library(curl)
+ocpu <- function(path, handle = new_handle(), server = 'http://localhost:5656/ocpu'){
   url <- file.path(server, path, fsep = "/")
-  curl::curl_fetch_memory(url, handle = curl::new_handle(...))
+  curl::curl_fetch_memory(url, handle = handle)
 }
 
-
 # Test post
-req <- ocpu('/library/base/R/identity/print', postfields = 'x=rnorm(10)')
+req <- ocpu('/library/base/R/identity/print', new_handle(postfields = 'x=rnorm(10)'))
+cat(rawToChar(req$content))
+
+# Test post multipart
+req <- ocpu('/library/base/R/identity/print', handle_setform(new_handle(), x = "rnorm(10)"))
 cat(rawToChar(req$content))
 
 # Test empty post
-req <- ocpu('/library/utils/R/sessionInfo/print', customrequest = 'POST')
+req <- ocpu('/library/utils/R/sessionInfo/print', new_handle(customrequest = 'POST'))
 cat(rawToChar(req$content))
+
+# Test multipart RDS object
+handle <- handle_setform(new_handle(), object = form_data(serialize(iris, NULL), "application/rds"))
+req <- ocpu('/library/base/R/summary/print', handle)
+cat(rawToChar(req$content))
+
+# Test multipart RProtoBuf object
+handle <- handle_setform(new_handle(), object = form_data(protolite::serialize_pb(iris), "application/rprotobuf"))
+req <- ocpu('/library/base/R/summary/print', handle)
+cat(rawToChar(req$content))
+
+# Test unspecified text types
+handle <- handle_setform(new_handle(), x = form_data("some text blabla", "text/foobar"))
+req <- ocpu('/library/base/R/identity/print', handle)
+cat(rawToChar(req$content))
+
+# Test unspecified binary types
+handle <- handle_setform(new_handle(), x = form_data("some text blabla", "application/foobar"))
+req <- ocpu('/library/base/R/identity/print', handle)
+cat(rawToChar(req$content))
+
+# Raw protobuf POST
+buf <- protolite::serialize_pb(list(n = 3, mean = 100))
+handle <- new_handle(postfields = buf)
+handle_setheaders(handle, 'Content-Type' = 'application/rprotobuf')
+req <- ocpu('/library/stats/R/rnorm/json', handle)
+cat(rawToChar(req$content))
+
+
+
+# Raw protobuf POST
+con <- rawConnection(buf)
+readfun <- function(n){readBin(con, raw(), n)}
+handle <- new_handle(readfunction = readfun, upload = TRUE, customrequest = 'POST')
+handle_setheaders(handle, 'Content-Type' = 'application/rprotobuf')
+req <- ocpu('/library/stats/R/rnorm/json', handle)
+cat(rawToChar(req$content))
+
