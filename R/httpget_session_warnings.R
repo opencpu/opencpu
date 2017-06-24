2fe7d53588a0104cbbbf26e6b104ff8f7c5ea08e
Jeroen <jeroenooms@gmail.com>
2017-06-24 21:17:07 +0200

    Tweak

--- R/httpget_session_warnings.R
+++ R/httpget_session_warnings.R
@@ -1,10 +1,10 @@
 httpget_session_warnings <- function(filepath, requri){
-  
+
   #load data
-  myeval <- readRDS(sessionfile <- file.path(filepath, ".REval"));
+  myeval <- readRDS(file.path(filepath, ".REval"));
   mywarnings <- extract(myeval, "warning");
   mywarnings <- lapply(mywarnings, "[[", "message");
-  
+
   #render
   reqformat <- requri[1];
   httpget_object(mywarnings, reqformat, "warnings", "text");
