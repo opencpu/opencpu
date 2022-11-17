9d85c019d99d2a9ba40e93ecdfdd9b54e3adab80
Panagiotis Cheilaris <philaris@gmail.com>
2022-11-17 14:29:41 +0100

    do not deparse length 1 named vector (#413)

--- R/parse_post.R
+++ R/parse_post.R
@@ -55,7 +55,9 @@ parse_post <- function(reqbody, contenttype){
   }
 
   return(lapply(obj, function(x){
-    if(is.null(x) || isTRUE(is.atomic(x) && length(x) == 1 && !length(dim(x)))){
+    if(is.null(x) ||
+       isTRUE(is.atomic(x) && length(x) == 1 &&
+              !length(dim(x))) && is.null(names(x))){
       #primitives as expressions
       return(deparse_atomic(x))
     } else {
